4937dda92316840e04cc22cba8642c079549ccae
HARADA Makoto <harada@toydea.com>
2019-07-07 18:38:25 +0900

    Add empty visual studio project.

---
+++ MyLang/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    /// <summary>
+    /// デバッガがアタッチされている場合は、キーの入力を待つ
+    ///
+    /// Windowsの開津環境で、コンソールがすぐに閉じてしまうのの対策として使用している
+    /// </summary>
+    static void waitKey()
+    {
+        if (Debugger.IsAttached)
+        {
+            Console.ReadKey();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        waitKey();
+    }
+}
+
