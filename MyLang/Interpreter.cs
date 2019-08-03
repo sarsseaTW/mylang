3da21c3225a2e4d3fe1674e42742a96f9da45a6b
HARADA Makoto <harada@toydea.com>
2019-08-04 01:40:33 +0900

    fibが動くところまで

--- MyLang/Interpreter.cs
+++ MyLang/Interpreter.cs
@@ -13,14 +13,23 @@ namespace MyLang
 
         float returnValue_;
 
-        public Env()
-        {
+        Env parentEnv_;
 
+        public Env(Env parentEnv)
+        {
+            parentEnv_ = parentEnv;
         }
 
         public float Get(string name)
         {
-            return variables_[name];
+            if (variables_.TryGetValue(name, out var found))
+            {
+                return found;
+            }
+            else
+            {
+                return parentEnv_.Get(name);
+            }
         }
 
         public void Set(string name, float val)
@@ -30,7 +39,14 @@ namespace MyLang
 
         public Ast.FunctionStatement GetFunction(string name)
         {
-            return functions_[name];
+            if (functions_.TryGetValue(name, out var found))
+            {
+                return found;
+            }
+            else
+            {
+                return parentEnv_.GetFunction(name);
+            }
         }
 
         public void SetFunction(string name, Ast.FunctionStatement func)
@@ -55,7 +71,7 @@ namespace MyLang
         public class BreakJump : InterpreterJump { }
         public class ReturnJump : InterpreterJump { }
 
-        Env env_ = new Env();
+        Env env_ = new Env(null);
 
         public Interpreter()
         {
@@ -149,34 +165,7 @@ namespace MyLang
         {
             if( exp is BinOp)
             {
-                var binop = exp as BinOp;
-                float lhsValue = runExp(binop.Lhs);
-                float rhsValue = runExp(binop.Rhs);
-                switch( binop.Operator)
-                {
-                    case BinOpType.Add:
-                        return lhsValue + rhsValue;
-                    case BinOpType.Sub:
-                        return lhsValue - rhsValue;
-                    case BinOpType.Multiply:
-                        return lhsValue * rhsValue;
-                    case BinOpType.Divide:
-                        return lhsValue / rhsValue;
-                    case BinOpType.Equal:
-                        return (lhsValue == rhsValue) ? 1: 0;
-                    case BinOpType.NotEqual:
-                        return (lhsValue != rhsValue) ? 1 : 0;
-                    case BinOpType.Less:
-                        return (lhsValue < rhsValue) ? 1 : 0;
-                    case BinOpType.LessEqual:
-                        return (lhsValue <= rhsValue) ? 1 : 0;
-                    case BinOpType.Greater:
-                        return (lhsValue > rhsValue) ? 1 : 0;
-                    case BinOpType.GreaterEqual:
-                        return (lhsValue >= rhsValue) ? 1 : 0;
-                    default:
-                        throw new Exception($"Unkonwn operator {binop.Operator}");
-                }
+                return runBinOp(exp as BinOp);
             }
             else if( exp is Number)
             {
@@ -190,30 +179,74 @@ namespace MyLang
             }
             else if (exp is ApplyFunction)
             {
-                var e = exp as ApplyFunction;
-                var func = env_.GetFunction(e.Name.Name);
-                var args = e.Args.Select(arg => runExp(arg)).ToArray();
-
-                for (int i = 0; i < args.Length; i++) {
-                    var arg = args[i];
-                    env_.Set("@"+(i+1), arg);
-                }
-
-                try
-                {
-                    runBlock(func.Body);
-                }
-                catch (ReturnJump)
-                {
-                    // return によって終了する
-                }
-
-                return env_.GetReturnValue();
+                return runApplyFunction(exp as ApplyFunction);
             }
             else
             {
                 throw new Exception("BUG");
             }
+        }
+
+        float runBinOp(BinOp binop)
+        {
+            float lhsValue = runExp(binop.Lhs);
+            float rhsValue = runExp(binop.Rhs);
+            switch (binop.Operator)
+            {
+                case BinOpType.Add:
+                    return lhsValue + rhsValue;
+                case BinOpType.Sub:
+                    return lhsValue - rhsValue;
+                case BinOpType.Multiply:
+                    return lhsValue * rhsValue;
+                case BinOpType.Divide:
+                    return lhsValue / rhsValue;
+                case BinOpType.Equal:
+                    return (lhsValue == rhsValue) ? 1 : 0;
+                case BinOpType.NotEqual:
+                    return (lhsValue != rhsValue) ? 1 : 0;
+                case BinOpType.Less:
+                    return (lhsValue < rhsValue) ? 1 : 0;
+                case BinOpType.LessEqual:
+                    return (lhsValue <= rhsValue) ? 1 : 0;
+                case BinOpType.Greater:
+                    return (lhsValue > rhsValue) ? 1 : 0;
+                case BinOpType.GreaterEqual:
+                    return (lhsValue >= rhsValue) ? 1 : 0;
+                default:
+                    throw new Exception($"Unkonwn operator {binop.Operator}");
+            }
+        }
+
+        float runApplyFunction(ApplyFunction e)
+        {
+            var func = env_.GetFunction(e.Name.Name);
+            var args = e.Args.Select(arg => runExp(arg)).ToArray();
+
+            // closureの環境を作成する
+            var oldEnv = env_;
+            env_ = new Env(oldEnv);
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                var arg = args[i];
+                env_.Set("@" + (i + 1), arg);
+            }
+
+            try
+            {
+                runBlock(func.Body);
+            }
+            catch (ReturnJump)
+            {
+                // return によって終了する
+            }
+
+            var returnValue = env_.GetReturnValue();
+
+            env_ = oldEnv;
+
+            return returnValue;
         }
     }
 }
